f134101cceccbb25b11e9900e3eae38dc1052f81
张荟萱 <pku2000012955@pku.edu.cn>
2023-03-25 10:28:10 +0800

    added res-conv

--- Decoder.py
+++ Decoder.py
@@ -3,10 +3,24 @@ import torch
 import MLP
 
 class Decoder(nn.Module):
-    def __init__(self, dim, dropout):
+    def __init__(self, token_dim, num_heads, dropout):
         nn.Module.__init__(self)
-        self.self_att1 = nn.MultiheadAttention(dim, 4, dropout)
-        
+        self.self_att1 = nn.MultiheadAttention(token_dim, 4, dropout)
+        self.ln1 = nn.LayerNorm((16 * 16, token_dim))
+        self.token_dim = token_dim
+        self.conv1 = nn.
+    
     def forward(self, input):
+        '''
+            decode the embedded features into image
+            input: embedded feature, [bsz, 16 * 16, token_dim]
+        '''
         att_output, _ = self.self_att1(input, input)
+        res_output = att_output + input
+        ln1_output = self.ln1(res_output)
+        
+        channel_output = ln1_output.transpose(1, 2).view(-1, self.token_dim, 16, 16)
+        
+        
+        
     
---
+++ res_conv.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+import torch
+
+class Res_Conv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, with_normalization, 
+                 activation = 'relu', *args):
+        nn.Module.__init__(self)
+        self.conv = nn.Conv2d(in_channels= in_channels, out_channels= out_channels, 
+                              kernel_size= kernel_size, padding= (kernel_size - 1) // 2)
+        if self.with_normalization:
+            self.norm = nn.BatchNorm2d(out_channels)
+        else:
+            self.norm = None
+        
+        if self.activation == 'relu':
+            self.act = nn.ReLU()
+        elif self.activation == 'leakyrelu':
+            self.act = nn.LeakyReLU(args)
+        elif self.activation == 'sigmoid':
+            self.act = nn.Sigmoid()
+        elif self.activation == 'tanh':
+            self.act = nn.Tanh()
+        elif self.activation == 'elu':
+            self.act = nn.ELU()
+        else:
+            self.act = None
+        
+                
+        
+    def forward(self, input):
+        output = self.conv(input)
+        
+        if self.norm:
+            output = self.norm(output)
+        
+        if self.act:
+            output = self.act(output)
+            
+        return input + output #res connection
